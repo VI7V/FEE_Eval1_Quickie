29c3ffe86837687311852c9fd4eef45ff6fbe60c
udaybir6 <143985062+udaybir6@users.noreply.github.com>
2024-10-24 10:22:02 +0530

    Update script.js

--- Products/script.js
+++ Products/script.js
@@ -58,3 +58,19 @@ function toggleMenu() {
 function closeMenu() {
     document.getElementById("sideMenu").style.width = "0";
 }
+function search() {
+    let filter = document.getElementById('find').value.toUpperCase();
+    let item = document.querySelectorAll('.product');
+    let l = document.getElementsByTagName('h3');
+    for(var i = 0;i<=l.length;i++){
+    let a=item[i].getElementsByTagName('h3')[0];
+    let value=a.innerHTML || a.innerText || a.textContent;
+    if(value.toUpperCase().indexOf(filter) > -1) {
+    item[i].style.display="";
+    }
+    else
+    {
+    item[i].style.display="none";
+    }
+    }
+}
